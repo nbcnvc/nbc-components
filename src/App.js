21d94eee4325696ba3b401b0ec80639d57285332
nbcnvc <nbcnvc.dev@gmail.com>
2023-06-22 14:14:49 +0900

    Add Button component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,27 @@ import Button from "./components/Button/Button";
 function App() {
   return (
     <>
-      <h2>haha</h2>
+      <h2>Button</h2>
+      <Button color="green" size="big" primary>
+        Large primary Button
+      </Button>
+      <Button color="green" size="big">
+        Big
+      </Button>
+      <Button color="green" size="small">
+        Small
+      </Button>
+      <br />
+      <br />
+      <Button color="red" size="big" primary>
+        Large primary Button
+      </Button>
+      <Button color="red" size="big">
+        Big
+      </Button>
+      <Button color="red" size="small">
+        Small
+      </Button>
     </>
   );
 }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,35 @@
-const Button = () => {
-  return <button>button</button>
-}
+import { styled } from "styled-components";
+
+const NbcButton = styled.button`
+  border: ${(props) => (props.primary ? `2px solid ${props.color}` : "none")};
+  border-radius: 8px;
+  background: ${(props) => (props.primary ? "transparent" : props.color)};
+  padding: ${(props) => btnSizes[props.size].padding};
+  font-size: ${(props) => btnSizes[props.size]["font-size"]};
+  margin-right: 10px;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+`;
+
+const btnSizes = {
+  small: {
+    padding: "8px 12px",
+    "font-size": "10px",
+  },
+  big: {
+    padding: "10px 30px",
+    "font-size": "14px",
+  },
+};
+
+const Button = (props) => {
+  const { size, color, children, primary } = props;
+  return (
+    <>
+      <NbcButton color={color} size={size} primary={primary}>
+        {children}
+      </NbcButton>
+    </>
+  );
+};
 
 export default Button;
